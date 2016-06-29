4d74b4225f0ca5ab8c4747fb7d2c4cb1eb0edb58
Dean Shaff <dshaff001@gmail.com>
2016-06-29 13:40:46 -0400

    Finished up dataset creation

--- dataset.py
+++ dataset.py
@@ -2,27 +2,49 @@ import numpy as np
 import theano.tensor as T
 import theano 
 import h5py
+import time 
+from preprocessor import TextProcessor
 
 
-class Dataset(object):
+class TextDataset(object):
 
-    def __init__(self, filename):
+    def __init__(self, filename,**kwargs):
         """
         args:
             -filename: should be an hdf5 file 
         """
-        self.filename 
-        
+        self.filename = filename  
+        self.get_data(**kwargs)  
 
-    def get_data(self):
+    def get_data(self,shared=False):
         """
-        Grab the data from the dataset file. 
-        Store it into two Theano shared variables.
+        Grab the data from the dataset file and turn it into one hot vectors.
+        kwargs:
+            -shared: If true, load data into shared variable.s 
         """
+        print("Loading data into memory")
+        t0 = time.time() 
         f = h5py.File(self.filename,'r') 
         dat = f['dat'][...]
-        input_dat = dat[:-1]
-        obs_dat = dat[1:]
-        input_dat = theano.shared(input_dat)
-        obs_dat = theano.shared(obs_dat) 
-
+        print("Data loaded from hdf5 file. Took {:.2f} seconds".format(time.time() - t0))
+        chars = f.attrs['chars'] 
+        dat_one_hot = np.zeros((dat.shape[0], len(chars)),dtype=theano.config.floatX) #need float because we're doing regression 
+        indices = np.arange(dat.shape[0]) 
+        dat_one_hot[indices,dat] = 1.0 
+        print("One hot vectors generated. Took {:.2f} second".format(time.time() - t0))
+        input_dat = dat_one_hot[:-1]
+        obs_dat = dat_one_hot[1:]
+        if shared:
+            input_dat = theano.shared(input_dat)
+            obs_dat = theano.shared(obs_dat) 
+            print("Theano shared variables created. Took {:.2f} seconds.".format(time.time()  - t0))
+        else:
+            pass 
+        f.close() 
+        self.dat = dat 
+        self.data_one_hot = dat_one_hot 
+        self.obs_dat = obs_dat 
+        self.input_dat = input_dat
+if __name__ == '__main__':
+    filename = 'shakespeare.hdf5'
+    txter = TextDataset(filename) 
--- preprocessor.py
+++ preprocessor.py
@@ -2,33 +2,24 @@ import numpy as np
 import h5py
 import time 
 
-class PreProcessor(object):
+class TextProcessor(object):
 
-    def __init__(self, source_file):
+    def __init__(self,source_file):
+        self.source_file = source_file
+
+    def grab_dat(self):
+        """
+        Grab data from the source file and turn it into a string of indices that correspond
+        to characters in a master list. 
         
-        self.source_file = source_file 
-
-    def grab_dat(self):
-        pass
-
-    def save_dat(self,filename):
-        pass
-
-
-class TextProcessor(PreProcessor):
-
-    def __init__(self,*args):
-
-        super(TextProcessor,self).__init__(*args)
-
-    def grab_dat(self):
+        """
         dat_str = str()
         with open(self.source_file,'r') as reader:
             for line in reader:
                 dat_str += line 
 
         unique_char = list(set(dat_str))
-        unique_char.sort() #sort it 
+        unique_char.sort()  
         char_dict = {char:i for i, char in enumerate(unique_char)}
         dat_small = np.zeros((len(dat_str)),dtype=int)
         for i,char in enumerate(dat_str):
@@ -37,11 +28,12 @@ class TextProcessor(PreProcessor):
         return dat_small, char_dict, unique_char
 
     def save_dat(self,filename):
+
         dat_small, char_dict, unique_char = self.grab_dat() 
         t0 = time.time()
         print("Saving dataset") 
         f= h5py.File(filename, 'w') 
-        f.create_dataset('datsmall',data=dat_small)
+        f.create_dataset('dat',data=dat_small)
         f.attrs['chars'] = unique_char 
         f.close() 
         print("Took {:.2f} seconds to save dataset".format(time.time()-t0))
